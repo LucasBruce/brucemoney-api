ee53b8e14944598c944b23ccc4290c3378e511ed
Lucas-Bruce <lucasmatheusbruce@gmail.com>
2021-10-09 08:51:23 -0300

    first commit

---
+++ brucemoney-api/src/main/java/com/example/brucemoneyapi/BrucemoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.brucemoneyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrucemoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrucemoneyApiApplication.class, args);
+	}
+
+}
---
+++ brucemoney-api/src/test/java/com/example/brucemoneyapi/BrucemoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brucemoneyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrucemoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
