f7afac0df3aa440da4db38da4974ee5918a9108f
LucasBruce <lucasmatheusbruce@gmail.com>
2021-10-16 09:06:16 -0300

    backup

--- brucemoney-api/src/main/java/com/example/brucemoneyapi/resource/CategoriaResource.java
+++ brucemoney-api/src/main/java/com/example/brucemoneyapi/resource/CategoriaResource.java
@@ -1,6 +1,5 @@
 package com.example.brucemoneyapi.resource;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,6 +7,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,44 +17,42 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.example.brucemoneyapi.event.RecursoCriadoEvent;
 import com.example.brucemoneyapi.model.Categoria;
 import com.example.brucemoneyapi.repository.CategoriaRepository;
 
 @RestController
 @RequestMapping("/categorias")
-public class CategoriaResource{
+public class CategoriaResource {
 
 	/*
-	 * #Status code:
-	 *  #2xx -> Sucesso 
-	 *  #4xx -> Erro do cliente 
-	 *  #5xx -> Erro no serviço/servidor
+	 * #Status code: #2xx -> Sucesso #4xx -> Erro do cliente #5xx -> Erro no
+	 * serviço/servidor
 	 */
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
+	@Autowired
+	private ApplicationEventPublisher publisher;
+
 	@GetMapping
-	public List<Categoria> findAll(){
+	public List<Categoria> findAll() {
 		return this.categoriaRepository.findAll();
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response, BindingResult result) {
-		
+	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response,
+			BindingResult result) {
 		Categoria categoriaSalva = categoriaRepository.save(categoria);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-	    .buildAndExpand(categoriaSalva.getCodigo()).toUri();
-		response.setHeader("Location", uri.toASCIIString());
-		return ResponseEntity.created(uri).body(categoriaSalva);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
 	}
-	
-	
+
 	@GetMapping("/{codigo}")
-	public Optional<Categoria> buscaPeloCodigo(@PathVariable Long codigo) {
+	public ResponseEntity<Optional<Categoria>> buscaPeloCodigo(@PathVariable Long codigo) {
 		Optional<Categoria> categoria = categoriaRepository.findById(codigo);
-		return categoria;
+		return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
 	}
 }
--- brucemoney-api/src/main/java/com/example/brucemoneyapi/resource/PessoaResource.java
+++ brucemoney-api/src/main/java/com/example/brucemoneyapi/resource/PessoaResource.java
@@ -1,6 +1,5 @@
 package com.example.brucemoneyapi.resource;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,16 +7,22 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.example.brucemoneyapi.event.RecursoCriadoEvent;
 import com.example.brucemoneyapi.model.Pessoa;
 import com.example.brucemoneyapi.repository.PessoaRepository;
 
@@ -27,6 +32,9 @@ public class PessoaResource {
 
 	@Autowired
 	private PessoaRepository pessoaRepository;
+
+	@Autowired
+	private ApplicationEventPublisher publisher;
 
 	@GetMapping
 	public List<Pessoa> lista() {
@@ -38,16 +46,20 @@ public class PessoaResource {
 	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response,
 			BindingResult result) {
 		Pessoa pessoaSalvar = this.pessoaRepository.save(pessoa);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/codigo")
-				.buildAndExpand(pessoaSalvar.getCodigo()).toUri();
-		response.setHeader("Location", uri.toASCIIString());
-		return ResponseEntity.created(uri).body(pessoaSalvar);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalvar.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalvar);
 	}
 
 	@GetMapping("/{codigo}")
-	public Optional<Pessoa> buscarPessoa(@PathVariable Long codigo) {
+	public ResponseEntity<Optional<Pessoa>> buscarPessoa(@PathVariable Long codigo) {
 		Optional<Pessoa> pessoa = this.pessoaRepository.findById(codigo);
-		return pessoa;
+		return pessoa != null ? ResponseEntity.ok(pessoa) : ResponseEntity.notFound().build();
+	}
+
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		this.pessoaRepository.deleteById(codigo);
 	}
 
 }
